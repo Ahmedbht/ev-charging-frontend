ae1a84198da885bb8596560de3c6f3d2d5509aee
Ahmed <email@gmail.com>
2026-06-04 19:27:09 +0100

    add search bae

--- src/components/StationList.jsx
+++ src/components/StationList.jsx
@@ -1,12 +1,14 @@
 //useState and useEffect
 import{ useState, useEffect} from "react";
 import axios  from "axios";
-import{MapPin, Zap,User,CheckCircle, HelpCircle} from "lucide-react";
+import{MapPin, Zap,User,CheckCircle, HelpCircle, Search} from "lucide-react";
 
 function StationList(){
     //stations list start empty// and data still loading 
     const[stations, setStations]= useState([]);
     const[loading, setLoading] =useState(true);
+    const[search, setSearch] = useState("");
+
 
     useEffect(()=>{
         axios.get("http://127.0.0.1:8000/stations")
@@ -24,16 +26,23 @@ function StationList(){
     if (loading){
         return<p>Loading stations..</p>;
     }
+    const filtredStations = stations.filter((station)=> station.city?.toLowerCase().includes(search.toLowerCase()))
 
     return(
-        <div className="Stations List">
-            <h2>EV Charging Stations in Morocco</h2>
+        <div className="station-list">
+            <h2>EV Charging Stations inA Morocco</h2>
 
+
+            <div className="search-bar">
+                <Search size={18} />
+                <input type="text" placeholder="Search by city:" value={search} onChange={(e) => setSearch(e.target.value)}/>
+            </div>
+            <p className="results-count ">{filtredStations.length} Stations found</p>
             {/*loop for each stations and show it as cards*/}
-            {stations.map((station) =>
+            {filtredStations.map((station) =>
             (
                 <div key={station.id} className="station-card">
-                    <h3>{stations.name}</h3>
+                    <h3>{station.name}</h3>
                     <p><MapPin size={16} /> City:{station.city || "Unknown"}</p>
                     <p><User size={16} /> Operator: {station.operator || "Unknown"}</p>
                     <p> <Zap size={16} /> Chargin Points:{station.number_of_points || "Unknown"}</p>
